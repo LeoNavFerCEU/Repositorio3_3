d4b66a6f40a0e3704bab1827daae0e4097f89a03
LeoNavFerCEU <lnavarro8494@ceuandalucia.es>
2023-11-24 20:16:18 +0100

    Añadida clase "Clase3_3A".

---
+++ Proyecto 3_3/src/Clase3_3A.java
@@ -0,0 +1,14 @@
+
+public class Clase3_3A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int addition = 0;
+		for (int count = 0; count < 10; count++) {
+			count ++;
+			addition += count;
+		}
+		System.out.println(addition);
+	}
+
+}
